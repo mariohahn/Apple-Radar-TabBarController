7c05d3b4bb2b7682b8fdfcae7bf7fb409ff93a4c
Mario Hahn <mario_hahn@me.com>
2018-11-19 16:00:44 +0100

    Initial Commit

---
+++ Apple-Radar/Apple-Radar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Apple-Radar
+//
+//  Created by Mario Hahn on 19.11.18.
+//  Copyright © 2018 Mario Hahn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
